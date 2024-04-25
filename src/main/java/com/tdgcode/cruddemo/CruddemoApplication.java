e9ed94d793b89046c8064c500704d54b96dec8bd
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-04-25 14:47:57 -0300

    execute the StudentDAO and check in the DB

--- src/main/java/com/tdgcode/cruddemo/CruddemoApplication.java
+++ src/main/java/com/tdgcode/cruddemo/CruddemoApplication.java
@@ -5,6 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import com.tdgcode.cruddemo.dao.StudentDAO;
+import com.tdgcode.cruddemo.dao.StudentDAOImpl;
+import com.tdgcode.cruddemo.entity.Student;
+
 @SpringBootApplication
 public class CruddemoApplication {
 
@@ -13,10 +17,20 @@ public class CruddemoApplication {
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner(String[] args) {
+	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 		return runner -> {
-			System.out.println("Hello World!");
+			createStudent(studentDAO);
 		};
 	}
 
+	private void createStudent(StudentDAO studentDAO) {
+		System.out.println("Creating new student object...");
+		Student tempStudent = new Student("Mano", "Silve", "mano@mail.com");
+
+		System.out.println("Saving the student...");
+		studentDAO.save(tempStudent);
+
+		System.out.println("Saved student. Generated id: " + tempStudent.getId());
+
+	}
 }
