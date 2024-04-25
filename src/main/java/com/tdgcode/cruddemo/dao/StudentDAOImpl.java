20d0b2131b0d9bc5f44b20ff0f2d823ebfa2764a
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-04-25 15:33:49 -0300

    add a new method to the StudentDAO

--- src/main/java/com/tdgcode/cruddemo/dao/StudentDAOImpl.java
+++ src/main/java/com/tdgcode/cruddemo/dao/StudentDAOImpl.java
@@ -1,11 +1,14 @@
 package com.tdgcode.cruddemo.dao;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.tdgcode.cruddemo.entity.Student;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import jakarta.transaction.Transactional;
 
 @Repository
@@ -27,5 +30,12 @@ public class StudentDAOImpl implements StudentDAO {
 	@Override
 	public Student findById(Integer id) {
 		return this.entityManager.find(Student.class, id);
+	}
+
+	@Override
+	public List<Student> findAll() {
+		TypedQuery<Student> theQuery = this.entityManager.createQuery("FROM Student", Student.class);
+
+		return theQuery.getResultList();
 	}
 }
